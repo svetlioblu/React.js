be4115234615399a59209dac91f418d0ef5f54c1
Svetoslav Genchev <svetlioblu@yahoo.com>
2023-03-25 21:25:21 +0200

    project init

---
+++ trip/src/App.js
@@ -0,0 +1,14 @@
+import { Routes, Route } from 'react-router-dom'
+
+function App() {
+  return (
+    <>
+      <body id="body" data-spy="scroll" data-target=".one-page-header" class="demo-lightbox-gallery">
+        {/* <Routes> <Route path='/' element={<Home />} </Routes> */}
+      </body>
+
+    </>
+  )
+}
+
+export default App
